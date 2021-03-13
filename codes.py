afd51b719ed42f3dd8923e05ab2f0f2cd02dacd2
innTall <troll999@hot.ee>
2021-03-13 09:50:52 +0100

    first

---
+++ codes.py
@@ -0,0 +1,16 @@
+from autobahn.twisted.websocket import WebSocketServerProtocol
+from binance.client import Client
+from binance.websockets import BinanceSocketManager
+
+client = Client()
+bm = BinanceSocketManager(client)
+# start any sockets here, i.e a trade socket
+
+def process_message(msg):
+  print("message type: {}".format(msg['e']))
+  print(msg)
+  # do something
+
+conn_key = bm.start_trade_socket('ASRBTC', process_message)
+# then start the socket manager
+bm.start()
